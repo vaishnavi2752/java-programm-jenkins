af616bd1632afe02b027dd94cad128e00a5de400
vaishnavi2752 <127757987+vaishnavi2752@users.noreply.github.com>
2026-03-24 16:23:37 +0530

    Update Hello.java

--- Jenkins/src/Main/Hello.java
+++ Jenkins/src/Main/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 public static void main(String[] args) {
 	System.out.println("hello everyone");
 	System.out.println("welcome to jenkins");
-	System.out.println(new Date);
+	System.out.println(new Date());
 }
 }
